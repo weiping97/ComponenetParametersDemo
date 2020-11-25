3c21ba69807451a167eb94591672b896fd167ded
weiping97 <weipingyap97@gmail.com>
2020-11-25 22:49:01 +0800

    Add project files.

---
+++ BlazorApp/Models/AddressModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp.Models
+{
+    public class AddressModel
+    {
+        public string AddressType { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ BlazorApp/Pages/UserInfo/Directory.cs
@@ -0,0 +1,66 @@
+﻿using BlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages.UserInfo
+{
+    public partial class Directory
+    {
+        public List<PersonModel> People { get; set; }
+        public Directory()
+        {
+            People = new List<PersonModel>
+        {
+            new PersonModel
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                Addresses = new List<AddressModel>
+                {
+                    new AddressModel
+                    {
+                        AddressType = "Home Address",
+                        StreetAddress = "123 Oak Street",
+                        City = "Scranton",
+                        State = "PA",
+                        ZipCode = "18512"
+                    },
+                    new AddressModel
+                    {
+                        AddressType = "Vacation Home",
+                        StreetAddress = "101 Beachfront Ave",
+                        City = "Miami",
+                        State = "FL",
+                        ZipCode = "25412"
+                    }
+                }
+            },
+            new PersonModel
+            {
+                FirstName = "Bob",
+                LastName = "Smith",
+                Addresses = new List<AddressModel>()
+
+            },
+            new PersonModel
+            {
+                FirstName = "Sue",
+                LastName = "Storm",
+                Addresses = new List<AddressModel>
+                {
+                    new AddressModel
+                    {
+                        AddressType = "Home Address",
+                        StreetAddress = "115 Justice Way",
+                        City = "LA",
+                        State = "CA",
+                        ZipCode = "88547"
+                    }
+                }
+            }
+        };
+        }
+    }
+}
